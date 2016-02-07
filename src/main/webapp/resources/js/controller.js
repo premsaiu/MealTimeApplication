f96aefba4a3479b478e211fe12e950d95ace37b1
Narendra Tirupati <narendra.tirupati@yahoo.com>
2016-02-07 21:50:04 +0530

    Code to hide the image on cancel

--- src/main/webapp/resources/js/controller.js
+++ src/main/webapp/resources/js/controller.js
@@ -32,10 +32,18 @@ controller('AboutUsCtrl', function ($scope,$http) {
   
 controller('AmMealCtrl', function ($scope,$http) {
 	
-		$scope.status = "Today's Breakfast Special";
+	var brkfstObj = {
+			status: "Today's Breakfast Special",
+			imgSrc: "resources/images/ammeal/meal_01.jpg",
+			imageName: "Masala Dosa",
+			brkfstInfo: "Masala Dosa with Chutney"
+	}
+	
+	$scope.brkfstObj = brkfstObj;
+		/*$scope.status = "Today's Breakfast Special";
 		$scope.imageSrc = "resources/images/ammeal/meal_01.jpg";
 		$scope.imageName = "Masala Dosa";
-		$scope.brkfstInfo = "Masala Dosa with Chutney";
+		$scope.brkfstInfo = "Masala Dosa with Chutney";*/
 		
 		var totalAmt = 1000;
 		
@@ -64,32 +72,135 @@ controller('AmMealCtrl', function ($scope,$http) {
 		}];
 		
 		var finalAmt = 0;
-		$scope.updateSelection = function(){
+		
+		$scope.chooseItems = function(){
+			angular.forEach($scope.complItems, function(value,key){
+				if(value.selected){
+					value.selected = false;
+				}
+			});
+			
+			$scope.favoriteSuppl = "";
+			$scope.showModal = true;
+			
+			$("#myAddonModal").modal('show');
+		};
+		$scope.cancelled = false;
+		$scope.cancelAllItems = function(brkfstObj){
+			delete $scope.brkfstObj;
+			angular.element(brkfstObj).attr("imgSrc","");
+			$scope.cancelled = true;
+			//$scope.brkfstObj.imgSrc = "";
+			$scope.show = false;
+		}
+		
+		
+		$scope.updateSelection = function(event){
 			$scope.supplflag = false;
 			$scope.alertShow = false;
-			angular.forEach($scope.complItems, function(value,key){
+			
+			/*angular.forEach($scope.complItems, function(value,key){
 				if(value.selected){
 					$scope.supplflag = true;
 				}
+			});*/
+			
+			angular.forEach($scope.suppleItems, function(value,key){
+				if(value.Item==$scope.favoriteSuppl){
+					$scope.supplflag = true;
+				}
 			});
+			
+			if ($scope.supplflag) {
+				if(confirm("Do you want to replace the selected item with existing?")){
+					angular.forEach($scope.suppleItems, function(value,key){
+						if(value.Item==$scope.favoriteSuppl){
+							//value.selected = false;
+							$scope.supplflag = false;
+							$scope.favoriteSuppl = "";
+						}
+					});
+				}else{
+					event.preventDefault();
+					//$scope.supplflag = true;
+					angular.forEach($scope.complItems, function(value,key){
+						if(value.selected){
+							value.selected = false;
+						}
+					});
+				}
+			}
 		}
 		
-		$scope.favoriteSuppl = "" ;
+		$scope.favoriteSuppl = "";
 
-		$scope.updateSelectionSuppl = function(){
+		$scope.updateSelectionSuppl = function(event){
 			$scope.complflag = false;
 			$scope.alertShow = false;
-			angular.forEach($scope.suppleItems, function(value,key){
+			
+			/*angular.forEach($scope.suppleItems, function(value,key){
 				if(value.Item==$scope.favoriteSuppl){
 					$scope.complflag = true;
 				}
+			});*/
+			
+			angular.forEach($scope.complItems, function(value,key){
+				if(value.selected){
+					$scope.complflag = true;
+				}
 			});
+			
+			if ($scope.complflag) {
+				if(confirm("Do you want to replace the selected item with existing?")){
+					angular.forEach($scope.complItems, function(value,key){
+						if(value.selected){
+							value.selected = false;
+							$scope.complflag = false;
+						}
+					});
+				}else{
+					event.preventDefault();
+		k8			//$scope.complflag = true;
+					$scope.favoriteSuppl = "";
+				}
+			}
 		}
+		
+		$(document).ready(function() {
+		    
+		    /*
+		    * I like wrapping inputs with labels for various reasons
+		    * What gets targeted may have to change depending on your markup
+		    * I'm using mouseup for the event as it seems more reliable and "click"
+		    * triggers two events
+		    */
+		    $("#new label").on("mouseup", function(e) {
+		        
+		        // cache the selected radio item
+		        var thisItem = $(this);
+		        // save the current state of the chosen item
+		        var previous = thisItem.children("input").prop("checked");
+		        // if item was previously selected then...
+		        if (previous) {
+		            // I introduce a slight delay
+		            // Without the delay the input stays checked
+		            setTimeout(function() {
+		                // after the delay uncheck the item
+		                // call blur() at end in case of focus indicator on input
+		                thisItem.children("input").prop("checked", false).blur();
+		            }, 10);
+		        }
+
+		    });       
+		    
+		});
+		
 		
 		$scope.addFinalItems = function(){
 			$scope.show = false;
 			$scope.finalData = [];
 			var totalAmt = 1000;
+			
 			angular.forEach($scope.complItems, function(value,key){
 				if(value.selected){
 					$scope.show = true;
