a7b42048b6709e3efa7252cadc644ca15e9e0136
Narendra Tirupati <narendra.tirupati@yahoo.com>
2016-02-17 08:30:49 +0530

    Seperated the Controllers

---
+++ src/main/webapp/resources/js/aboutusctrl.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('miniMealApp.controllers', []).
+
+	controller('AboutUsCtrl', function ($scope,$http) {
+	
+	alert("In About Us Controller");
+	
+  });
---
+++ src/main/webapp/resources/js/paymentctrl.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('miniMealApp.controllers', []).
+	controller('PaymentCtrl', function ($scope,$http) {
+	
+	alert("In Payment Controller");
+	
+});
