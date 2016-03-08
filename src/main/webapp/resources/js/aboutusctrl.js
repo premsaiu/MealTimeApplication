1fe95e5da536db9d3bc7165afc42a8ef18769501
Narendra Tirupati <narendra.tirupati@ge.com>
2016-03-08 11:43:11 +0530

    Removed Used Files

--- src/main/webapp/resources/js/aboutusctrl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('miniMealApp.controllers', []).
-
-	controller('AboutUsCtrl', function ($scope,$http) {
-	
-	alert("In About Us Controller");
-	
-  });
--- src/main/webapp/resources/js/paymentctrl.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('miniMealApp.controllers', []).
-	controller('PaymentCtrl', function ($scope,$http) {
-	
-	alert("In Payment Controller");
-	
-});
