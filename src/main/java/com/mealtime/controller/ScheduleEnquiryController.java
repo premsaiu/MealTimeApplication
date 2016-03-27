be1a4b9dd0366d7b13b4d7b8a1360929b377e8b2
premsaiu <u.premsai@gmail.com>
2016-03-27 23:16:38 +0530

    [Prem] Schedule Enquiry Updated

--- src/main/java/com/mealtime/controller/ScheduleEnquiryController.java
+++ src/main/java/com/mealtime/controller/ScheduleEnquiryController.java
@@ -17,12 +17,23 @@ public class ScheduleEnquiryController {
 	@Autowired
 	private ScheduleEnquiryService scheduleEnquiryService;
 	
+	@Autowired
+	MealTimeUtil mealTimeUtil;
+	
 	@RequestMapping("/scheduleEnquiry")
 	public @ResponseBody WSResponseStatus scheduleEnquiry(@RequestBody ScheduleEnquiry scheduleEnquiry){
 		WSResponseStatus wsResponseStatus = new WSResponseStatus();
 		boolean isScheduleEnquiry = scheduleEnquiryService.scheduleEnquiry(scheduleEnquiry);
 		if(isScheduleEnquiry){
 			MealTimeUtil.populateWSResponseStatusSuccessResponse(wsResponseStatus);
+			String message = "Hi, Your Schedule Enquiry has been fixed. Our team will visit you shortly.";
+			//mealTimeUtil.sendSMS(scheduleEnquiry.getMobileNumber(), message);
+			/*String subject = "MealTime - Schedule Enquriy";
+			String msgBody = "<i>Hi!</i><br><br>";
+			msgBody += "<b>Welcome to MealTime!</b><br>";
+			msgBody += "Your Schedule Enquiry has been fixed. Our team will visit you shortly.<br><br>";
+			msgBody += "Regards, <br>Meal Time Team";
+			mealTimeUtil.sendEmail(scheduleEnquiry.getEmail(), "premcse41@gmail.com", subject, msgBody);*/
 		}else{
 			MealTimeUtil.populateWSResponseStatusFailsureStatusResponse(wsResponseStatus, "Failed to Save in table");
 		}
--- src/main/java/com/mealtime/service/ScheduleEnquiryService.java
+++ src/main/java/com/mealtime/service/ScheduleEnquiryService.java
@@ -3,21 +3,28 @@ package com.mealtime.service;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.mealtime.bean.ScheduleEnquiry;
 import com.mealtime.dao.ScheduleEnquiryDAO;
 
+@Service
 public class ScheduleEnquiryService {
 	
 	@Autowired
 	private ScheduleEnquiryDAO scheduleEnquiryDAO;
 	
 	public boolean scheduleEnquiry(ScheduleEnquiry scheduleEnquiry){
-		System.out.println("Date Time :: "+scheduleEnquiry.getScheduleDateTime());
-		scheduleEnquiry.setCreatedDate(new Date());
-		scheduleEnquiry.setIsActive("YES");
-		scheduleEnquiryDAO.insert(scheduleEnquiry);
-		return false;
+		try{
+			System.out.println("Date Time :: "+scheduleEnquiry.getScheduleDateTime());
+			scheduleEnquiry.setCreatedDate(new Date());
+			scheduleEnquiry.setIsActive("YES");
+			scheduleEnquiryDAO.insert(scheduleEnquiry);
+			return true;
+		}catch(Exception e){
+			System.out.println("Exception while saving:: "+e.getMessage());
+			return false;
+		}
 	}
 
 }
