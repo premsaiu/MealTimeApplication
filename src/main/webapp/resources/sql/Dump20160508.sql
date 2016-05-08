041b356d9401507e191bde25c341c0f0fbe7d50b
Prem Sai <u.premsai@gmail.com>
2016-05-08 15:29:44 +0530

    [Prem] Added new SQL

--- src/main/webapp/resources/sql/Dump20160508.sql
+++ src/main/webapp/resources/sql/Dump20160508.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `mealtime` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE  IF NOT EXISTS `mealtime` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `mealtime`;
--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
 --
--- Host: localhost    Database: mealtime
+-- Host: 127.0.0.1    Database: mealtime
 -- ------------------------------------------------------
--- Server version	5.7.11-log
+-- Server version	5.6.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -158,6 +158,35 @@ INSERT INTO `am_updated_sub_items` VALUES (13,4,'MT003','2016-03-13',NULL,NULL,N
 UNLOCK TABLES;
 
 --
+-- Table structure for table `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `feedback` (
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `mobile_number` varchar(100) NOT NULL,
+  `comments` varchar(250) DEFAULT NULL,
+  `created_date` date DEFAULT NULL,
+  `created_by` varchar(100) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `is_active` varchar(10) DEFAULT NULL,
+  `version` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feedback`
+--
+
+LOCK TABLES `feedback` WRITE;
+/*!40000 ALTER TABLE `feedback` DISABLE KEYS */;
+/*!40000 ALTER TABLE `feedback` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `meals_master`
 --
 
@@ -211,7 +240,7 @@ CREATE TABLE `otp_table` (
 
 LOCK TABLES `otp_table` WRITE;
 /*!40000 ALTER TABLE `otp_table` DISABLE KEYS */;
-INSERT INTO `otp_table` VALUES ('u.premsai@gmail.com','9700640022','986845',NULL,NULL,'2016-02-21 18:23:00'),('narendra.tirupati@yahoo.com','9494663543','767397',NULL,NULL,'2016-03-13 10:46:17'),('bharath.dhanpal@gmail.com','9010889853','542334',NULL,NULL,'2016-02-11 05:25:14'),('undefined','9700640000','165322',NULL,NULL,'2016-02-21 18:36:03'),('prem2.sai2@gmail.com','9700600000','878210',NULL,NULL,'2016-02-21 18:55:02'),('admin@mealtime.com','9700640011','288103',NULL,NULL,'2016-03-04 18:11:18'),('prem3.sai3@gmail.com','9700640023','200522',NULL,NULL,'2016-03-04 18:58:15'),('premcse41@gmail.com','9700640024','405836',NULL,NULL,'2016-03-04 18:59:30');
+INSERT INTO `otp_table` VALUES ('u.premsai@gmail.com','9700640022','812659',NULL,NULL,'2016-03-31 10:13:53'),('narendra.tirupati@yahoo.com','9494663543','767397',NULL,NULL,'2016-03-13 05:16:17'),('bharath.dhanpal@gmail.com','9010889853','542334',NULL,NULL,'2016-02-10 23:55:14'),('undefined','9700640000','165322',NULL,NULL,'2016-02-21 13:06:03'),('prem2.sai2@gmail.com','9700600000','878210',NULL,NULL,'2016-02-21 13:25:02'),('admin@mealtime.com','9700640011','288103',NULL,NULL,'2016-03-04 12:41:18'),('prem3.sai3@gmail.com','9700640023','200522',NULL,NULL,'2016-03-04 13:28:15'),('premcse41@gmail.com','9700640024','405836',NULL,NULL,'2016-03-04 13:29:30'),('prem4.sai4@gmail.com','9700640044','277458',NULL,NULL,'2016-03-13 14:47:40'),('prem5.sai5@gmail.com','9700640055','207782',NULL,NULL,'2016-03-14 18:01:58'),('prem6.sai6@gmail.com','9700640066','267944',NULL,NULL,'2016-03-14 18:43:39'),('prem7.sai7@gmail.com','9700640077','237473',NULL,NULL,'2016-03-16 17:22:55'),('null','9700640088','310469',NULL,NULL,'2016-03-29 14:18:00'),('null','9700640099','740131',NULL,NULL,'2016-03-31 10:01:14'),('null','9700640012','575998',NULL,NULL,'2016-05-08 09:25:11');
 /*!40000 ALTER TABLE `otp_table` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -356,6 +385,82 @@ INSERT INTO `pm_updated_sub_items` VALUES (16,2,'MT003','2016-03-13',NULL,NULL,N
 UNLOCK TABLES;
 
 --
+-- Table structure for table `sample_meal`
+--
+
+DROP TABLE IF EXISTS `sample_meal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sample_meal` (
+  `sample_meal_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(7) NOT NULL,
+  `sample_meal_date` date NOT NULL,
+  `mobile_number` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `created_date` date DEFAULT NULL,
+  `updated_date` date DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `updated_by` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `is_active` varchar(255) DEFAULT NULL,
+  `version` int(10) DEFAULT NULL,
+  PRIMARY KEY (`sample_meal_id`),
+  UNIQUE KEY `mobile_number` (`mobile_number`),
+  KEY `fk_sample_meal_user_id` (`user_id`),
+  CONSTRAINT `fk_sample_meal_user_id` FOREIGN KEY (`user_id`) REFERENCES `user_master` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sample_meal`
+--
+
+LOCK TABLES `sample_meal` WRITE;
+/*!40000 ALTER TABLE `sample_meal` DISABLE KEYS */;
+INSERT INTO `sample_meal` VALUES (1,'MT014','2016-03-30','9700640088','Prem Sai','madhapur','2016-03-29',NULL,NULL,NULL,'Open','YES',NULL),(6,'MT015','2016-03-30','9700640099','Prem Sai','madhapur','2016-03-29',NULL,NULL,NULL,'Open','YES',NULL);
+/*!40000 ALTER TABLE `sample_meal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `schedule_enquiry`
+--
+
+DROP TABLE IF EXISTS `schedule_enquiry`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `schedule_enquiry` (
+  `enquiry_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(7) NOT NULL,
+  `schedule_date_time` datetime NOT NULL,
+  `mobile_number` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `created_date` date DEFAULT NULL,
+  `updated_date` date DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `updated_by` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `is_active` varchar(255) DEFAULT NULL,
+  `version` int(10) DEFAULT NULL,
+  PRIMARY KEY (`enquiry_id`),
+  UNIQUE KEY `mobile_number` (`mobile_number`),
+  KEY `fk_enquiry_user_id` (`user_id`),
+  CONSTRAINT `fk_enquiry_user_id` FOREIGN KEY (`user_id`) REFERENCES `user_master` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `schedule_enquiry`
+--
+
+LOCK TABLES `schedule_enquiry` WRITE;
+/*!40000 ALTER TABLE `schedule_enquiry` DISABLE KEYS */;
+INSERT INTO `schedule_enquiry` VALUES (1,'MT015','2018-07-03 15:30:14','9700640099','Prem','madhapur','2016-03-31',NULL,NULL,NULL,NULL,'YES',NULL),(2,'MT016','2016-05-09 14:00:00','9700640012','Prem','Madhapur','2016-05-08',NULL,NULL,NULL,NULL,'YES',NULL);
+/*!40000 ALTER TABLE `schedule_enquiry` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `subscription_master`
 --
 
@@ -396,7 +501,7 @@ DROP TABLE IF EXISTS `user_master`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_master` (
   `user_id` varchar(7) NOT NULL DEFAULT '0',
-  `first_name` varchar(255) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `mobile_number` varchar(255) NOT NULL,
@@ -413,9 +518,10 @@ CREATE TABLE `user_master` (
   `food_style_s1` varchar(250) DEFAULT NULL,
   `food_style_s2` varchar(250) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
+  `dinner_choice` varchar(100) DEFAULT NULL,
+  `packing_choice` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `mobile_number` (`mobile_number`),
-  UNIQUE KEY `email` (`email`),
   KEY `fk_role_id` (`role_id`),
   CONSTRAINT `fk_role_id` FOREIGN KEY (`role_id`) REFERENCES `user_role_master` (`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -427,9 +533,30 @@ CREATE TABLE `user_master` (
 
 LOCK TABLES `user_master` WRITE;
 /*!40000 ALTER TABLE `user_master` DISABLE KEYS */;
-INSERT INTO `user_master` VALUES ('MT002','Prem','Sai','u.premsai@gmail.com','9700640022','Flat No. 1211, Suraram Colony Main Road, Suraram, Jeedimetla, Hyderabad - 500055',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT002.jpg','non-veg','south',NULL),('MT003','Narendra','Tirupati','narendra.tirupati@yahoo.com','9494663543','KNR Homes, Flat No. 207, Bahadurpally, Hyderabad-500043',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\gegdc\\PS336409\\Software\\apache-tomcat-8.0.15\\MealTime_User_Images\\9494663543.jpg','veg','south',NULL),('MT004','Bharath','Dhanpal','bharath.dhanpal@gmail.com','9010889853','Suchitra, Hyderabad',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\gegdc\\PS336409\\Software\\apache-tomcat-8.0.15\\MealTime_User_Images\\9010889853.jpg','veg','south',NULL),('MT005','Prem1','Sai1','premcse41@gmail.com','9700640000','Ellamma Street, Santhapet, Chittoor',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT004.jpg','Veg','North',NULL),('MT006','Prem2','Sai2','prem2.sai2@gmail.com','9700600000','Chittoor',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT006.jpg','Veg','North',NULL),('MT008','Prem','Admin','admin@mealtime.com','9700640011','Flat No. 1211, Suraram Colony Main Road, Suraram, Jeedimetla, Hyderabad - 500055',NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT008.jpg','veg','north','prem ias'),('MT009','Prem3','Sai3','prem3.sai3@gmail.com','9700640023','Suraram, Hyderabad',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT009.jpg','Veg','South',NULL);
+INSERT INTO `user_master` VALUES ('MT002','Prem','Sai','u.premsai@gmail.com','9700640022','Flat No. 1211, Suraram Colony Main Road, Suraram, Jeedimetla, Hyderabad - 500055',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\gegdc\\PS336409\\Software\\apache-tomcat-8.0.15\\MealTime_User_Images\\MT002.jpg','non-veg','south',NULL,NULL,NULL),('MT003','Narendra','Tirupati','narendra.tirupati@yahoo.com','9494663543','KNR Homes, Flat No. 207, Bahadurpally, Hyderabad-500043',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\gegdc\\PS336409\\Software\\apache-tomcat-8.0.15\\MealTime_User_Images\\9494663543.jpg','veg','south',NULL,NULL,NULL),('MT004','Bharath','Dhanpal','bharath.dhanpal@gmail.com','9010889853','Suchitra, Hyderabad',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\gegdc\\PS336409\\Software\\apache-tomcat-8.0.15\\MealTime_User_Images\\9010889853.jpg','veg','south',NULL,NULL,NULL),('MT005','Prem1','Sai1','premcse41@gmail.com','9700640000','Ellamma Street, Santhapet, Chittoor',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT004.jpg','Veg','North',NULL,NULL,NULL),('MT006','Prem2','Sai2','prem2.sai2@gmail.com','9700600000','Chittoor',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT006.jpg','Veg','North',NULL,NULL,NULL),('MT008','Prem','Admin','admin@mealtime.com','9700640011','Flat No. 1211, Suraram Colony Main Road, Suraram, Jeedimetla, Hyderabad - 500055',NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT008.jpg','veg','north','PremASE@14',NULL,NULL),('MT009','Prem3','Sai3','prem3.sai3@gmail.com','9700640023','Suraram, Hyderabad',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT009.jpg','Veg','South',NULL,NULL,NULL),('MT010','Prem4','Sai4','prem4.sai4@gmail.com','9700640044','Hyderabad',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT010.jpg','Veg','South',NULL,NULL,NULL),('MT011','Prem5','Sai5','prem5.sai5@gmail.com','9700640055','Hyderabad',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT011.jpg','Veg','South',NULL,NULL,NULL),('MT012','Prem6','Sai6','prem6.sai6@gmail.com','9700640066','Hyderabad',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT012.jpg','Veg','North',NULL,NULL,NULL),('MT013','Prem7','Sai7','prem7.sai7@gmail.com','9700640077','Hyderabad',NULL,NULL,NULL,NULL,NULL,NULL,NULL,2,'D:\\Softwares\\apache-tomcat-8.0.30\\MealTime_User_Images\\MT013.jpg','Veg','South',NULL,NULL,NULL),('MT014',NULL,NULL,NULL,'9700640088',NULL,'2016-03-24',NULL,NULL,NULL,'Visitor','NO',NULL,3,NULL,NULL,NULL,NULL,NULL,NULL),('MT015',NULL,NULL,NULL,'9700640099',NULL,'2016-03-29',NULL,NULL,NULL,'Visitor','NO',NULL,3,NULL,NULL,NULL,NULL,NULL,NULL),('MT016',NULL,NULL,NULL,'9700640012',NULL,'2016-05-08',NULL,NULL,NULL,'Visitor','NO',NULL,3,NULL,NULL,NULL,NULL,NULL,NULL);
 /*!40000 ALTER TABLE `user_master` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = latin1 */ ;
+/*!50003 SET character_set_results = latin1 */ ;
+/*!50003 SET collation_connection  = latin1_swedish_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER tg_user_master_insert
+BEFORE INSERT ON user_master
+FOR EACH ROW
+BEGIN
+  INSERT INTO user_master_seq VALUES (NULL);
+  SET NEW.user_id = CONCAT('MT', LPAD(LAST_INSERT_ID(), 3, '0'));
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `user_master_seq`
@@ -441,7 +568,7 @@ DROP TABLE IF EXISTS `user_master_seq`;
 CREATE TABLE `user_master_seq` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -450,7 +577,7 @@ CREATE TABLE `user_master_seq` (
 
 LOCK TABLES `user_master_seq` WRITE;
 /*!40000 ALTER TABLE `user_master_seq` DISABLE KEYS */;
-INSERT INTO `user_master_seq` VALUES (2),(3),(4),(5),(6),(8),(9);
+INSERT INTO `user_master_seq` VALUES (2),(3),(4),(5),(6),(8),(9),(10),(11),(12),(13),(14),(15),(16);
 /*!40000 ALTER TABLE `user_master_seq` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -508,7 +635,7 @@ CREATE TABLE `user_role_master` (
 
 LOCK TABLES `user_role_master` WRITE;
 /*!40000 ALTER TABLE `user_role_master` DISABLE KEYS */;
-INSERT INTO `user_role_master` VALUES (1,'Admin','2016-02-10','2016-02-10','Prem','Prem',NULL,'YES',1),(2,'User','2016-02-10','2016-02-10','Prem','Prem',NULL,'YES',1);
+INSERT INTO `user_role_master` VALUES (1,'Admin','2016-02-10','2016-02-10','Prem','Prem',NULL,'YES',1),(2,'User','2016-02-10','2016-02-10','Prem','Prem',NULL,'YES',1),(3,'Visitor','2016-03-24','2016-03-24','Prem','Prem',NULL,'YES',1);
 /*!40000 ALTER TABLE `user_role_master` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -522,8 +649,10 @@ DROP TABLE IF EXISTS `user_subscription`;
 CREATE TABLE `user_subscription` (
   `user_id` varchar(7) NOT NULL,
   `subscription_id` int(11) NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date NOT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `created_date` date DEFAULT NULL,
+  `updated_date` date DEFAULT NULL,
   `created_by` varchar(255) DEFAULT NULL,
   `updated_by` varchar(255) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
@@ -535,7 +664,7 @@ CREATE TABLE `user_subscription` (
   KEY `fk_subscription_id` (`subscription_id`),
   CONSTRAINT `fk_subscription_id` FOREIGN KEY (`subscription_id`) REFERENCES `subscription_master` (`subscription_id`),
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user_master` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -544,7 +673,7 @@ CREATE TABLE `user_subscription` (
 
 LOCK TABLES `user_subscription` WRITE;
 /*!40000 ALTER TABLE `user_subscription` DISABLE KEYS */;
-INSERT INTO `user_subscription` VALUES ('MT002',1,'2016-03-06','2016-04-04',NULL,NULL,'In Progress','NO',NULL,1),('MT003',1,'2016-03-07','2016-04-04',NULL,NULL,'success','YES',1,2);
+INSERT INTO `user_subscription` VALUES ('MT010',1,NULL,NULL,'2016-03-13','2016-03-14','MT010','MT008','Success','Yes',NULL,3),('MT011',1,NULL,NULL,'2016-03-14','2016-03-14','MT011','MT008','Success','Yes',NULL,4),('MT012',1,'2016-01-16','2016-01-14','2016-03-15','2016-03-15','MT012','MT008','Success','Yes',NULL,5),('MT013',1,'2016-03-16','2016-04-14','2016-03-16','2016-03-16','MT013','MT008','Success','Yes',NULL,6);
 /*!40000 ALTER TABLE `user_subscription` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -608,9 +737,17 @@ CREATE TABLE `user_wallet` (
 
 LOCK TABLES `user_wallet` WRITE;
 /*!40000 ALTER TABLE `user_wallet` DISABLE KEYS */;
-INSERT INTO `user_wallet` VALUES ('MT003',895,NULL,NULL,'success','YES',1);
+INSERT INTO `user_wallet` VALUES ('MT003',895,NULL,NULL,'success','YES',1),('MT010',1000,'MT008',NULL,'Success','YES',NULL),('MT011',1000,'MT008',NULL,'Success','YES',NULL),('MT012',1000,'MT008',NULL,'Success','YES',NULL),('MT013',1000,'MT008',NULL,'Success','YES',NULL);
 /*!40000 ALTER TABLE `user_wallet` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'mealtime'
+--
+
+--
+-- Dumping routines for database 'mealtime'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -621,4 +758,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-13 16:41:46
+-- Dump completed on 2016-05-08 15:27:17
